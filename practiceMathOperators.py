1580f2e65e07214fe1081af9f3efd583c667dbbf
lezlyv <57326531+lezlyv@users.noreply.github.com>
2019-11-30 14:08:25 -0600

    Initial commit

---
+++ practiceMathOperators.py
@@ -0,0 +1,66 @@
+'''
+Created on Nov 30, 2019
+
+@author: ITAUser
+'''
+
+x = 4 + 4 
+
+print(x) 
+
+y = x + 1 
+
+print(y) 
+
+z = x + y
+
+print(z) 
+
+x = x + 1
+
+string1 = "something"
+
+string2 = " else"
+
+stringTogether = string1 + string2
+
+print(stringTogether)
+
+x = 4 - 4 
+
+print(x) 
+
+x = x - 1 
+
+print(x) 
+
+x = 2 * 2.1
+
+print(x) 
+
+string = "michael " * 4
+
+print(string) 
+
+x = 4 / 4
+
+print(x) 
+
+x = 4 // 4 
+
+print(x) 
+
+x = 9 // 4
+
+print(x) 
+
+x = 4 % 3
+
+print(x) 
+
+x = 2 ** 4
+
+print(x) 
+
+x = "stuff " + " and things"
+print(x) 
